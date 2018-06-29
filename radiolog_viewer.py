a8944d9fccbcbfca8e1a55ec41a80ed077c83c59
Tom Grundy <caver456@gmail.com>
2018-06-28 20:21:27 -0700

    prelim: delete panels working
    prelim: redo grid on panel delete is working

--- radiolog_viewer.py
+++ radiolog_viewer.py
@@ -50,8 +50,9 @@ import time
 from radiolog_viewer_ui import Ui_radiolog_viewer
 
 statusColorDict={}
-statusColorDict["At IC"]="22ff22"
-statusColorDict["In Transit"]="2222ff"
+statusColorDict["At IC"]=["22ff22","000000"]
+statusColorDict["In Transit"]=["2222ff","eeeeee"]
+statusColorDict["Waiting for Transport"]=["2222ff","eeeeee"]
 
 class MyWindow(QMainWindow,Ui_radiolog_viewer):
     def __init__(self,parent):
@@ -109,11 +110,11 @@ class MyWindow(QMainWindow,Ui_radiolog_viewer):
         self.csvFiles=[]
         self.log=[]
         self.callsigns=[]
-#         for panel in self.panels:
-#             self.removeDockWidget(self.docks[dock])
-#             self.docks[dock].setParent(None)
-#         for dockTableWidget in self.dockTableWidgets:
-#             self.dockTableWidgets[dockTableWidget].setParent(None)
+        for callsign in self.panels:
+            self.ui.gridLayout.removeWidget(self.panels[callsign])
+            self.panels[callsign].setParent(None)
+            self.panels[callsign].deleteLater()
+        self.panels={}
         self.readDir()
         self.watchedFile=self.csvFiles[0][0]
         # remove the pygtail offset file, if any, so pygtail will
@@ -124,6 +125,8 @@ class MyWindow(QMainWindow,Ui_radiolog_viewer):
 #         self.ui.watchedFileField.setText(self.watchedFile)
         print("  found "+self.watchedFile)
         self.refresh(throb=False)
+#         print("callsigns after refresh:"+str(self.callsigns))
+#         print("panels after refresh:"+str(self.panels))
         if self.latestCallsign!="":
             self.panels[self.latestCallsign].throb(self.latestColor)
 #         self.getGridLocations()
@@ -147,8 +150,8 @@ class MyWindow(QMainWindow,Ui_radiolog_viewer):
                         
                     # add new entry to the callsign's panel's table
                     p=self.panels[callsign]
-#                     p.statusWidget.setText(status)
-                    p.statusWidget.setStyleSheet("background-color:#"+statusColorDict.get(status,"eeeeee"))
+                    p.statusWidget.setText(status)
+                    p.statusWidget.setStyleSheet("color:#"+statusColorDict.get(status,["","000000"])[1]+";background-color:#"+statusColorDict.get(status,["eeeeee",""])[0])
                     t=p.tableWidget
                     newRowCount=t.rowCount()+1
                     t.setRowCount(newRowCount)
@@ -239,19 +242,8 @@ class MyWindow(QMainWindow,Ui_radiolog_viewer):
 #             pos.setX(pos.x()+dockWidth)
 
     def newPanel(self,callsign):
-        [row,col]=self.getFirstOpenGridLocation()
-        print("First available grid location: "+str([row,col]))
-#         maxRows=int(self.maxGridHeight/self.panelHeight)
-#         if self.panelRowCount>=maxRows:
-#             self.panelColCount=self.panelColCount+1
-#         else:
-#             self.panelRowCount=self.panelRowCount+1
-#         row=len(self.panels)%maxRows
-#         col=self.panelColCount
-#         gridHeight=50+self.panelHeight*self.panelRowCount
-#         gridWidth=20+self.panelWidth*self.panelColCount
         self.callsigns.append(callsign)
-        panel=Panel(self)
+        panel=Panel(self,callsign)
         self.panels[callsign]=panel
         panel.titleWidget.setText(callsign)
         t=panel.tableWidget
@@ -268,6 +260,50 @@ class MyWindow(QMainWindow,Ui_radiolog_viewer):
         t.setMaximumWidth(self.panelWidth-8)
         t.setMinimumWidth(self.panelWidth-8)
         t.setGeometry(QRect(0,0,300,200))
+        self.addPanelToGrid(panel)
+
+    def redoGrid(self):
+#         self.ui.gridLayoutWidget.deleteLater()
+#         self.ui.gridLayout.deleteLater()
+#         self.ui.gridLayoutWidget=None
+#         self.ui.gridLayout=None
+        
+        self.ui.gridLayoutWidget = QWidget(self)
+        self.ui.gridLayoutWidget.setGeometry(QRect(218, 102, 597, 689))
+        self.ui.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.ui.gridLayout = QGridLayout(self.ui.gridLayoutWidget)
+        self.ui.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.ui.gridLayout.setSpacing(2)
+        self.ui.gridLayout.setObjectName("gridLayout")
+        self.ui.gridLayoutWidget.show()
+        self.ui.gridLayoutWidget.raise_()
+
+#         for r in range(self.ui.gridLayout.rowCount()):
+#             for c in range(self.ui.gridLayout.columnCount()):
+#                 print("removing at row="+str(r)+" col="+str(c))
+#                 self.ui.gridLayout.removeItem(self.ui.gridLayout.itemAtPosition(r,c))
+                
+#         for panel in self.panels:
+#             print(" removing "+panel+" "+str(self.panels[panel]))
+#             self.ui.gridLayout.removeWidget(self.panels[panel])
+
+#         print("\nAfter close:")
+#         self.getFirstOpenGridLocation()
+        
+#             self.panels[panel].hide()
+#         self.ui.gridLayout.invalidate()
+#         self.ui.gridLayout.update()
+
+        for callsign in self.panels:
+            self.addPanelToGrid(self.panels[callsign])
+#             self.panels[callsign].show()
+#         self.ui.gridLayoutWidget.raise_()
+        
+        
+
+    def addPanelToGrid(self,panel):
+        [row,col]=self.getFirstOpenGridLocation()
+        print("adding panel "+panel.callsign+" at row="+str(row)+" col="+str(col))
         self.ui.gridLayout.addWidget(panel,row,col)
         s=max(self.ui.gridLayout.horizontalSpacing(),self.ui.gridLayout.verticalSpacing())
         gridHeight=(self.panelHeight+s)*self.ui.gridLayout.rowCount()-s
@@ -277,14 +313,14 @@ class MyWindow(QMainWindow,Ui_radiolog_viewer):
     def getFirstOpenGridLocation(self):
         rows=self.ui.gridLayout.rowCount()
         cols=self.ui.gridLayout.columnCount()
-        print("the grid currently has "+str(rows)+" rows and "+str(cols)+" columns")
+#         print("the grid currently has "+str(rows)+" rows and "+str(cols)+" columns")
 #         for c in range(1,cols+1):
         for c in range(cols):
 #             for r in range(1,min(rows,int(self.maxGridHeight/self.panelHeight))+1):
              for r in range(min(rows,int(self.maxGridHeight/self.panelHeight))+1):
-                print("checking row:"+str(r)+" col:"+str(c)+" = "+str(self.ui.gridLayout.itemAtPosition(r,c)))
+#                 print("checking row:"+str(r)+" col:"+str(c)+" = "+str(self.ui.gridLayout.itemAtPosition(r,c)))
                 if not self.ui.gridLayout.itemAtPosition(r,c):
-                    print("empty cell found!")
+#                     print("empty cell found at row="+str(r)+" col="+str(c))
                     return([r,c])
         return([0,c+1])
     
@@ -367,41 +403,42 @@ class MyWindow(QMainWindow,Ui_radiolog_viewer):
         self.parent.quit()
 
 
-# class MyDockWidget(QDockWidget):
-
-# class Panel(QTableWidget):
-#     def __init__(self,parent):
-#         super().__init__(1,3)
-#         self.parent=parent
-#         self.palette=QPalette()
-
 class Panel(QFrame):
-    def __init__(self,parent):
+    def __init__(self,parent,callsign):
         super().__init__()
         self.parent=parent
+        self.callsign=callsign
         font=QFont()
         font.setFamily("Segoe UI")
         font.setPointSize(14)
         font.setBold(True)        
         smallFont=QFont()
         smallFont.setFamily("Segoe UI")
-        smallFont.setPointSize(11)        
+        smallFont.setPointSize(9)        
         self.palette=QPalette()
         self.layout=QVBoxLayout(self)
-        self.titleBarLayout=QHBoxLayout(self)
+        self.titleBarLayout=QHBoxLayout()
         self.titleWidget=QLabel("label")
         self.titleWidget.setFont(font)
         self.statusWidget=QLabel()
         self.statusWidget.setFont(smallFont)
+        self.statusWidget.setAlignment(Qt.AlignHCenter|Qt.AlignVCenter)
         self.closeButton=QPushButton(self)
-        icon = QIcon()
-        icon.addPixmap(QPixmap(":/radiolog_viewer_ui/close.png"), QIcon.Normal, QIcon.Off)
-        self.closeButton.setIcon(icon)
-        self.closeButton.setIconSize(QSize(20, 20))
-        self.closeButton.setMinimumHeight(20)
-        self.closeButton.setMaximumHeight(20)
+        self.closeButton.setMinimumHeight(24)
+        self.closeButton.setMaximumHeight(24)
+        self.closeButton.setMinimumWidth(24)
+        self.closeButton.setMaximumWidth(24)
+        self.closeButton.setStyleSheet("border:none")
+        self.closeButton.clicked.connect(self.close)
+        self.closeButton.setStyleSheet("""
+            QPushButton:hover
+                {border-image:url(:/radiolog_viewer_ui/close_dark.png)}
+            QPushButton
+                {border-image:url(:/radiolog_viewer_ui/close.png)}
+            """)
         self.titleBarLayout.addWidget(self.titleWidget)
         self.titleBarLayout.addWidget(self.statusWidget)
+        self.titleBarLayout.insertSpacing(2,20)
         self.titleBarLayout.addWidget(self.closeButton)
         self.tableWidget=QTableWidget(0,3,self)
         self.tableWidget.setStyleSheet("background-color:white")
@@ -420,7 +457,18 @@ class Panel(QFrame):
         self.setMaximumHeight(self.parent.panelHeight)
         self.setMinimumWidth(self.parent.panelWidth)
         self.setMaximumWidth(self.parent.panelWidth)
-     
+    
+    def closeEvent(self,event):
+        print("closed panel "+self.callsign)
+        event.accept()
+#         self.deleteLater()
+        del self.parent.panels[self.callsign]
+        self.parent.callsigns.remove(self.callsign)
+#         self.parent.ui.gridLayout.removeWidget(self)
+        self.parent.redoGrid()
+#         self.parent.ui.gridLayoutWidget.repaint()
+
+        
     def throb(self,final=[255,255,255],n=0):
         # this function calls itself recursivly 25 times to throb the background blue->white
 #         rprint("throb:n="+str(n))
@@ -439,20 +487,9 @@ class Panel(QFrame):
         else:
 #             rprint("throb complete")
             self.throbTimer=None
-#             self.palette.setColor(QPalette.Background,QColor(255,255,255))
             self.palette.setColor(QPalette.Background,QColor(final[0],final[1],final[2]))
             self.setStyleSheet("background:rgb("+str(final[0])+","+str(final[1])+","+str(final[2])+")")
             self.setPalette(self.palette)
-            
-            
-# class Dock(QMdiSubWindow):
-#     def __init__(self,parent,*args):
-#         QMdiSubWindow.__init__(self,*args)
-#         self.parent=parent
-#     
-#     def closeEvent(self,event):
-#         print("closing")
-#         QTimer.singleShot(200,self.parent.ui.mdi.tileSubWindows)
         
         
 def main():
